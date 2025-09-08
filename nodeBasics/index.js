96e46b05c4dbec956c2484f6e413303e4daa1574
Ihor <sanesmind@gmail.com>
2025-09-08 15:14:38 +0300

    feat: added Chalk and index.js

---
+++ nodeBasics/index.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+
+console.log(chalk.blue("First usage of library!"));
+
+console.log(chalk.red("Test red!"));
+
+console.log(chalk.yellow("Test yellow!"));
+
+console.log(chalk.green("Test green!"));
