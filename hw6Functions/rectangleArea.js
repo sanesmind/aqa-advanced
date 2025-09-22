7c6ce0dda326f80c0bf8d433fc19fc5cec6ed421
Ihor <sanesmind@gmail.com>
2025-09-22 21:45:53 +0300

    feat:homework6 basic functions

---
+++ hw6Functions/rectangleArea.js
@@ -0,0 +1,20 @@
+function calculateAreaOfRectangle(width, height) {
+    return width * height;
+}
+
+console.log(calculateAreaOfRectangle(5, 10));
+
+/////////////////////////////////////////////////////////////////////
+const calcAreaOfRectExpression = function (width, height) {
+    return width * height;
+}
+
+console.log(calcAreaOfRectExpression(6, 10));
+
+
+////////////////////////////////////////////////////////////////////
+const calcAreaOfRectArrow = (width, height) => {
+    return width * height;
+}
+
+console.log(calcAreaOfRectArrow(7, 10));
