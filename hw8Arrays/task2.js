36e5d648f4fe0f5736213ecdc5b4c3f8f5d355ba
Ihor <sanesmind@gmail.com>
2025-09-28 21:36:46 +0300

    feat: hw8 learning arrays andtheir methods

---
+++ hw8Arrays/task2.js
@@ -0,0 +1,6 @@
+const startArray = [1, 2, 3, 4, 5]
+
+const doubledArray = startArray.map((number) => number * 2 );
+
+console.log(startArray);
+console.log(doubledArray);
---
+++ hw8Arrays/task6.js
@@ -0,0 +1,9 @@
+const numbersList = [1,10,14,2,4,5,43,34];
+
+let sortedNumbersList = structuredClone(numbersList);
+
+sortedNumbersList.sort((a, b) => a - b);
+
+
+console.log(numbersList);
+console.log(sortedNumbersList);
